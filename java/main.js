08a627fe4ef143469a8801dfc7b8cc58571ad1e7
Sahil11gosaliya <s_gosaliya@fanshaweonline.ca>
2023-03-27 09:27:17 -0400

    Design Added
    CSS Implemented with JS

---
+++ java/main.js
@@ -0,0 +1,57 @@
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read More"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read Less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+
+  function myFunction1() {
+    var dots = document.getElementById("dots1");
+    var moreText = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots1.style.display === "none") {
+      dots1.style.display = "inline";
+      btnText.innerHTML = "Read More"; 
+      moreText.style.display = "none";
+    } else {
+      dots1.style.display = "none";
+      btnText.innerHTML = "Read Less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
